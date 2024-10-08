7277dd3d4a653819e441bd8a3f48f83a274f4346
Alcides Turruellas <ayunierto@gmail.com>
2024-10-07 22:00:59 -0700

    stack navigator setup

--- src/AscencioTaxApp.tsx
+++ src/AscencioTaxApp.tsx
@@ -1,16 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {SafeAreaView, View} from 'react-native';
 import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 
-// Screens
-import LoginScreen from './presentation/screens/auth/LoginScreen';
-import RegisterScreen from './presentation/screens/auth/RegisterScreen';
-import ProfileScreen from './presentation/screens/settings/ProfileScreen';
-import HomeScreen from './presentation/screens/home/HomeScreen';
-import BookingScreen from './presentation/screens/booking/BookingScreen';
-import ResumeScreen from './presentation/screens/booking/ResumeScreen';
-import LoadingScreen from './presentation/loading/LoadingScreen';
+import 'react-native-gesture-handler';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {StackNavigator} from './presentation/natigation/StackNavigator';
 
 const theme = {
   ...DefaultTheme,
@@ -24,16 +19,17 @@ const theme = {
 
 export const AscencioTaxApp = () => {
   return (
-    <PaperProvider
-      theme={theme}
-      settings={{
-        icon: props => <IonIcon {...props} />,
-      }}>
-      {/* <SafeAreaView style={{flex: 1}}> */}
-      <View>
-        <LoadingScreen />
-      </View>
-      {/* </SafeAreaView> */}
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider
+        theme={theme}
+        settings={{
+          // eslint-disable-next-line react/no-unstable-nested-components
+          icon: props => <IonIcon {...props} />,
+        }}>
+        {/* <SafeAreaView style={{flex: 1}}> */}
+        <StackNavigator />
+        {/* </SafeAreaView> */}
+      </PaperProvider>
+    </NavigationContainer>
   );
 };
---
+++ src/presentation/natigation/StackNavigator.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from '../screens/auth/LoginScreen';
+import LoadingScreen from '../loading/LoadingScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
+import HomeScreen from '../screens/home/HomeScreen';
+import BookingScreen from '../screens/booking/BookingScreen';
+import ResumeScreen from '../screens/booking/ResumeScreen';
+import ProfileScreen from '../screens/settings/ProfileScreen';
+
+export type RootStackParams = {
+  LoadingScreen: undefined;
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+  ProfileScreen: undefined;
+  HomeScreen: undefined;
+  BookingScreen: undefined;
+  ResumeScreen: undefined;
+};
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="BookingScreen" component={BookingScreen} />
+      <Stack.Screen name="ResumeScreen" component={ResumeScreen} />
+    </Stack.Navigator>
+  );
+};
